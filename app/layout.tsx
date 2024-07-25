00a8585aceb64dd8020b083c627c66b57600cee6
NaturesClub <2105891@kiit.ac.in>
2024-07-25 23:27:16 +0530

    feat: theme provider added bug: dark mode not working

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,10 @@
 import type { Metadata } from "next";
 import { DM_Sans } from 'next/font/google'
 import "./globals.css";
-import { cn } from "@/lib/utils"
+import { ThemeProvider } from "@/providers/theme-providers"
 
-const fontSans = DM_Sans({
+
+const font = DM_Sans({
   subsets: ["latin"],
   variable: "--font-sans",
 })
@@ -21,11 +22,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={cn(
-        "min-h-screen bg-background font-sans antialiased",
-        fontSans.variable
-      )}
-      >{children}</body>
+      <body className={font.className}
+      >
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
